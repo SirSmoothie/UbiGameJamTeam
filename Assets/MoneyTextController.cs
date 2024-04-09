cd09470dfa6d49706fcc1de700d187f6a9bc937c
Rory Rouillon <Thegameredbaron@gmail.com>
2024-04-09 21:54:39 +1000

    Push Before DoTween

---
+++ Assets/MoneyTextController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyTextController : MonoBehaviour
+{
+    public GameObject iconGameObject;
+    public GameObject textGameObject;
+    
+    
+}
